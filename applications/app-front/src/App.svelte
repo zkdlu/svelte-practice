02802414052b94818d121c870ad6d14544f2f82e
zkdlu <zkdlu159951@gmail.com>
2022-02-04 00:48:33 +0900

    스벨트 문법 연습

--- applications/app-front/src/App.svelte
+++ applications/app-front/src/App.svelte
@@ -1,10 +1,65 @@
 <script>
+import { each } from "svelte/internal";
+import Component from "./Component.svelte";
+
+
 	export let name;
+	let buttonName = '잉'
+	let toggle = false;
+	let value = 0;
+	let numbers = [];
+
+	function enter() {
+		buttonName = '잉';
+	}
+
+	function leave() {
+		buttonName = '엥?';
+	}
+
+	function repeat() {
+		setInterval(() => {
+			value += 1;
+		}, 1000);
+	}
+
+	repeat();
+
+	for (let i = 0; i < 10; i++) {
+		numbers.push(i);
+	}
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<button on:click={() => { toggle = !toggle;}}
+		on:mouseenter={enter}
+		on:mouseleave={leave}>
+		{buttonName}
+	</button>
+	<div style="display: {toggle ? 'none' : 'block'};">
+		<div>
+			<h2>조건문</h2>
+			<p style="display:{value % 2 == 0 ? 'none' : 'block'}">{value}</p>
+			{#if value % 2 == 0}
+				<p>짝수</p>
+			{:else}
+				<p>홀수</p>
+			{/if}
+		</div>
+		<div>
+			<h2>반복문</h2>
+			{#each numbers as number}
+				{number}
+			{/each}
+		</div>
+		<div>
+			<Component {numbers}/>
+			<Component {numbers} reverse/>
+			<Component {numbers} slice='-2'/>
+			<Component {numbers} slice='0,4'/>
+		</div>
+	</div>
 </main>
 
 <style>
---
+++ applications/app-front/src/Component.svelte
@@ -0,0 +1,25 @@
+<script>
+import { each } from "svelte/internal";
+
+
+    export let numbers;
+    export let reverse;
+    export let slice;
+
+    let computedNumbers = [];
+
+    if (reverse) {
+        computedNumbers = [...numbers].reverse();
+    } else if (slice) {
+        computedNumbers = [...numbers].slice(...slice.split(','))
+    } else {
+        computedNumbers = [...numbers];
+    }
+
+</script>
+
+
+<h4>Component</h4>
+{#each computedNumbers as number}
+    {number}
+{/each}
